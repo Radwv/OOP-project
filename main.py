36841f428c8a6e6118da4a7b9ad96e7a6119a320
Radwa Mohamed <143282686+Radwv@users.noreply.github.com>
2023-08-30 17:46:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,56 +1,83 @@
-print("welcome to Bascutta Ice Cream")
+import tkinter as tk
+from tkinter import simpledialog
 
 class IceCreamShop:
     def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Ice Cream Shop")
+        self.customer_name = ""
         self.menu = []
         self.prices = {}
-        self.customer_name = ""
 
-    def ask_customer_name(self):
-        self.customer_name = input("What's your name? ")
+    def run(self):
+        self.get_customer_name()
+        self.print_welcome_message()
+        self.print_menu()
+        self.place_order()
+        self.root.mainloop()
+
+    def get_customer_name(self):
+        self.customer_name = simpledialog.askstring("Customer Name", "Enter your name:")
 
     def print_welcome_message(self):
-        print("Welcome", self.customer_name + "!")
+        welcome_label = tk.Label(self.root, text="Welcome " + self.customer_name + "!")
+        welcome_label.pack()
 
     def print_menu(self):
-        print("Ice Cream Menu:")
+        menu_frame = tk.Frame(self.root)
+        menu_frame.pack()
+
+        menu_label = tk.Label(menu_frame, text="Ice Cream Menu:")
+        menu_label.pack()
+
+        self.menu_listbox = tk.Listbox(menu_frame)
+        self.menu_listbox.pack()
+
         for item in self.menu:
-            print(item)
+            self.menu_listbox.insert(tk.END, item)
 
     def place_order(self):
-        self.ask_customer_name()
-        self.print_welcome_message()
+        order_frame = tk.Frame(self.root)
+        order_frame.pack()
 
-        start_order = input("Do you want to start making an order? (Yes , No) ")
-        if start_order.lower() == "yes":
-            self.print_menu()
-            order = []
-            total_cost = 0
-            while True:
-                choice = input("Choose the type of ice cream you would like (Or type 'done' to finish): ")
-                if choice == "done":
-                    break
-                order.append(choice)
-                total_cost += self.prices[choice]
+        order_label = tk.Label(order_frame, text="Your order:")
+        order_label.pack()
 
-                add_more = input("Do you want to add something else? (Yes , No) ")
-                if add_more.lower() == "yes":
-                    self.print_menu()
-                else:
-                    break
+        self.order_listbox = tk.Listbox(order_frame)
+        self.order_listbox.pack()
 
-            payment_method = input("Would you like the payment method to be cash or visa? ")
-            print("Your request has been registered successfully.")
-            print("Your order:", order)
-            print("Total cost:", total_cost)
-            print("Payment method:", payment_method)
-            print("Thank you, we are pleased to deal with you.", self.customer_name + "!")
+        self.total_cost = 0
 
-        else:
-            print("Thank you for your visit", self.customer_name + "!")
+        self.entry_ice_cream = tk.Entry(self.root)
+        self.entry_ice_cream.pack()
 
-# code
-shop = IceCreamShop()
-shop.menu = ["Vanilla","Chocolate","Strawberry","Mint Chocolate Chip","Cookies and Cream","Rocky Road","Butter Pecan","Coffee","Salted Caramel","Cookie Dough"]
-shop.prices = {"Vanilla": 20, "Chocolate": 25, "Strawberry": 25, "Mint Chocolate Chip": 35, "Cookies and Cream": 35, "Rocky Road": 30, "Butter Pecan": 30, "Coffee": 25, "Salted Caramel": 30, "Cookie Dough": 35}
-shop.place_order()
+        self.button_add = tk.Button(self.root, text="Add", command=self.add_ice_cream)
+        self.button_add.pack()
+
+        self.button_finish = tk.Button(self.root, text="Finish", command=self.finish_order)
+        self.button_finish.pack()
+
+    def add_ice_cream(self):
+        choice = self.entry_ice_cream.get()
+        if choice == "done":
+            self.finish_order()
+        elif choice in self.menu:
+            self.order_listbox.insert(tk.END, choice)
+            self.total_cost += self.prices[choice]
+            self.entry_ice_cream.delete(0, tk.END)
+
+    def finish_order(self):
+        payment_method = simpledialog.askstring("Payment Method", "Enter payment method:")
+        order_summary = "Customer Name: " + self.customer_name + "\n"
+        order_summary += "Order Items: " + ", ".join(self.order_listbox.get(0, tk.END)) + "\n"
+        order_summary += "Total Cost: $" + str(self.total_cost) + "\n"
+        order_summary += "Payment Method: " + payment_method
+
+        self.label_order_summary = tk.Label(self.root, text=order_summary)
+        self.label_order_summary.pack()
+
+if __name__ == "__main__":
+    shop = IceCreamShop()
+    shop.menu = ["Vanilla","Chocolate","Strawberry","Mint Chocolate Chip","Cookies and Cream","Rocky Road","Butter Pecan","Coffee","Salted Caramel","Cookie Dough"]
+    shop.prices = {"Vanilla": 20, "Chocolate": 25, "Strawberry": 25, "Mint Chocolate Chip": 35, "Cookies and Cream": 35, "Rocky Road": 30, "Butter Pecan": 30, "Coffee": 25, "Salted Caramel": 30, "Cookie Dough": 35}
+    shop.run()
